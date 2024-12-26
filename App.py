9160e8b48c86e61299a6a7a33b21f7032eadc105
MarkSherif132 <111676506+MarkSherif132@users.noreply.github.com>
2024-12-27 00:32:55 +0200

    Update App.py

--- App.py
+++ App.py
@@ -9,13 +9,22 @@ nltk.download('wordnet')
 model=pickle.load(open("model/model.pkl",'rb'))
 vectorizer=pickle.load(open("model/vectorizer.pkl",'rb'))
 
-st.text("sentiment analysis")
-state = st.button("predict")
+st.title("Hello, Welcome to my project ", anchor=None, help=None)
+st.header("_Sentiment Analysis_", anchor=None, help=None)
+
 text = st.text_input("please enter your review")
+state = st.button("Analyze my feedback :sunglasses:")
 
 token = helper.preprocessing_step(text)
 vectorized_data = vectorizer.transform([token])
 prediction = model.predict(vectorized_data)
 
 if state :
-    st.text(prediction)
+    if prediction == 1:
+        st.html("<div style = 'background-color:rgb(100, 200, 100);'> This review is positive </span> </div> ")
+    else:
+        st.html("<div style = 'background-color:rgb(220, 00, 00);'> This review is negative </span> </div> ")
+    st.write("Thanks for your feedback ")
+
+st.html(" <div style = 'margin-bottom:0px'><hr></div>")    
+st.write("BY MARK SHERIF :balloon:")
